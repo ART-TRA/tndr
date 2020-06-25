c68fe72f04c1165e489070b77604c9ab37d0d755
DArt <qubon50r@mail.ru>
2020-06-26 00:46:07 +0300

    Updates

---
+++ precache-manifest.a7c57b0127f615ac4cd975766f4ba6e2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae0e0aeac45f676324ead84f49fa7ad7",
+    "url": "/tndr/index.html"
+  },
+  {
+    "revision": "3117a27589968ac47ac3",
+    "url": "/tndr/static/css/main.4575d743.chunk.css"
+  },
+  {
+    "revision": "46c64f939beb02cbd8d8",
+    "url": "/tndr/static/js/2.c54be459.chunk.js"
+  },
+  {
+    "revision": "8bc88370bc2489d86f2f357f447ddb0b",
+    "url": "/tndr/static/js/2.c54be459.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3117a27589968ac47ac3",
+    "url": "/tndr/static/js/main.da931ea3.chunk.js"
+  },
+  {
+    "revision": "72c80b6357e8e8b9dac1",
+    "url": "/tndr/static/js/runtime-main.53f95a61.js"
+  },
+  {
+    "revision": "f1e9f25d352bc338fbd0c698d624d481",
+    "url": "/tndr/static/media/card.f1e9f25d.png"
+  },
+  {
+    "revision": "28f69c409a4cf1749a3d33a675f46b10",
+    "url": "/tndr/static/media/home.28f69c40.png"
+  },
+  {
+    "revision": "8bfee1d68ba06e110e533dbf922094ac",
+    "url": "/tndr/static/media/photo.8bfee1d6.png"
+  }
+]);
